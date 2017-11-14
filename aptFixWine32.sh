bfeb62897fc5fccab4f17080e902120c18bd2ab5
R5fs <RafaelRfs@users.noreply.github.com>
2017-11-14 16:26:08 -0500

    Create aptFixWine32.sh

---
+++ aptFixWine32.sh
@@ -0,0 +1,2 @@
+echo deb http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
+echo deb-src http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
