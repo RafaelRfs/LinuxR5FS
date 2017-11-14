7fc63431fd47a6b7b4b69ca4cacb0677d700b9ac
R5fs <RafaelRfs@users.noreply.github.com>
2017-11-14 16:31:14 -0500

    Update aptFixWine32.sh

--- aptFixWine32.sh
+++ aptFixWine32.sh
@@ -1,2 +1,4 @@
+mkdir /root/.wine/drive_c/
 echo deb http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
 echo deb-src http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list
+dpkg --add-architecture i386 && apt-get update && apt-get install wine32
