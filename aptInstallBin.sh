e5f5a7a93723dd2f3e43aa79da8c9993e9d06f40
R5fs <RafaelRfs@users.noreply.github.com>
2017-11-20 17:24:42 -0200

    Update aptInstallBin.sh

--- aptInstallBin.sh
+++ aptInstallBin.sh
@@ -8,5 +8,8 @@ sudo mv network-nmap $bin_dir
 sudo mv showifi $bin_dir
 sudo mv verser $bin_dir
 sudo mv ShowDb $bin_dir
+sudo mv iptab $bin_dir
+sudo mv setEtr $bin_dir
+sudo mv nmp $bin_dir
+sudo mv nsp $bin_dir
 sudo mv ShowDbLocal $bin_dir
-
