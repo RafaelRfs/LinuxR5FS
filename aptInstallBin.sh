f4ca63592c3b265effedf5440faea48230d2645c
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-12-15 09:16:14 -0500

    Update aptInstallBin.sh

--- aptInstallBin.sh
+++ aptInstallBin.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
 bin_dir=/usr/bin/
 echo "[+]Instalando binarios..."
-sudo mv Client $bin_dir
-sudo mv Server $bin_dir
-sudo mv mit $bin_dir
-sudo mv network $bin_dir
-sudo mv network-nmap $bin_dir
-sudo mv showifi $bin_dir
-sudo mv verser $bin_dir
-sudo mv ShowDb $bin_dir
-sudo mv iptab $bin_dir
-sudo mv setEtr $bin_dir
-sudo mv nmp $bin_dir
-sudo mv nsp $bin_dir
-sudo mv ShowDbLocal $bin_dir
-sudo mv unscn $bin_dir
+mv Client $bin_dir
+mv Server $bin_dir
+mv mit $bin_dir
+mv network $bin_dir
+mv network-nmap $bin_dir
+mv showifi $bin_dir
+mv verser $bin_dir
+mv ShowDb $bin_dir
+mv iptab $bin_dir
+mv setEtr $bin_dir
+mv nmp $bin_dir
+mv nsp $bin_dir
+mv ShowDbLocal $bin_dir
+mv unscn $bin_dir
+mv setWallpaper $bin_dir
